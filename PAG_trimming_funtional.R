c5e9acabf97fe0cd86690c1f9975520b487524c0
김현규 / Kim Hyeonkyu <61915842+hg0426@users.noreply.github.com>
2022-09-02 17:38:30 +0900

    Add files via upload

---
+++ PAG_trimming_funtional.R
@@ -0,0 +1,15 @@
+library(stringr)
+library(dplyr)
+
+source("~/function_PAG_trimming.r")
+
+total_peak_gene <- read.delim("~/total_peak_gene_hg38.txt")
+target <- c("CD4","CD8","mono","Bcell")
+
+for (i in target){
+  print(i)
+  peak_list <- read.delim(paste0("C:/Users/LSCS/Desktop/tenet/0421/",i,"/TE_result_matrix_rowTF_colPK.fdr0.01.sif"), header=FALSE)
+  gene_list <- read.delim(paste0("C:/Users/LSCS/Desktop/tenet/0421/",i,"/TE_result_matrix_rowTF_colGN.fdr0.01.sif"), header=FALSE)
+}
+
+a <- trimming_PAG(total_peak_gene = total_peak_gene,peak_list = peak_list,gene_list = gene_list)
